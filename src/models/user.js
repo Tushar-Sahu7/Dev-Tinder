b90db4133e42e90e574bf50d9c7b8f45cf33569a
Tushar-Sahu7 <sahutushar532@gmail.com>
2026-03-29 17:12:17 +0530

    Added validator.js for validating User Schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -15,10 +16,20 @@ const userSchema = new mongoose.Schema({
     unique: true,
     lowercase: true,
     trim: true,
+    validate(value){
+      if(!validator.isEmail(value)){
+        throw new Error("Invalid email address!")
+      }
+    }
   },
   password: {
     type: String,
     required: true,
+    validate(value){
+      if(!validator.isStrongPassword(value)){
+        throw new Error("Strong password is required!")
+      }
+    }
   },
   age: {
     type: Number,
@@ -37,6 +48,11 @@ const userSchema = new mongoose.Schema({
 
   photoUrl: {
     type: String,
+    validate(value){
+      if(!validator.isURL(value)){
+        throw new Error("Invalid Photo URL!")
+      }
+    }
   },
   about: {
     type: String,
