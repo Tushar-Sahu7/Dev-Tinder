16ae519cd881cfc7767b4c33614d644d4217aee1
Tushar-Sahu7 <sahutushar532@gmail.com>
2026-05-23 23:29:07 +0530

    added(connectionRequest api): made send request api like a interns happy path with no validations at all

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequestModel = require("../models/connectionRequest");
 
 const requestRouter = express.Router();
 
 requestRouter.post(
-  "/request/send/interested/:toUserId",
+  "/request/send/:status/:toUserId",
   userAuth,
-  async (req, res) => {},
+  async (req, res) => {
+    const fromUserId = req.user._id;
+    const toUserId = req.params.toUserId;
+    const status = req.params.status;
+
+    const ConnectionRequest = new ConnectionRequestModel({
+      fromUserId,
+      toUserId,
+      status,
+    });
+    const data = await ConnectionRequest.save();
+
+    return res.status(201).json({
+      message: "Connection request send successfully",
+      data,
+    });
+  },
 );
 
 module.exports = requestRouter;
